0488f1c7fb7c31ed94edd99c4a758058cc8c2c9f
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2023-12-16 21:34:43 +0200

    Regex form validate
    - Implemented Regular Expression for email form with JS.
    
    - Assigned a class for error and valid email message display.

--- jscode.js
+++ jscode.js
@@ -1,21 +1,50 @@
 // Storing DOM in const
-const form = document.querySelector('#emailForm');
-const emailInput = document.getElementById('email');
-const msgText = document.getElementById('msg');
-const btnAction = document.querySelector('.notify-btn');
+const form = document.getElementById('emailForm');
+const email = document.getElementById('email');
+const errormsg = email.nextElementSibling;
 
-btnAction.addEventListener('click', onSubmit => {
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if (emailInput.value == '' || emailInput.value.trim() === '') {
-        msgText.innerHTML = 'Please provide a valid email address';
-        emailInput.classList.add('redBox');
+window.addEventListener('load', () => {
+
+    //Testing the inputfield if its empty | if not empty check for correct email formatting
+    const isValid = email.value === 0 || emailRegex.test(email.value);
+    //Condition context: if isValid true then valid else invalid
+    email.className = isValid ? "valid" : "invalid";
+
+});
+
+//Check for user input if not empty or has required regex then remove errormsg
+email.addEventListener('input', () => {
+    const isValid = email.value.length === 0 || emailRegex.test(email.value);
+    if (isValid) {
+        email.className = "valid";
+        errormsg.textContent = "";
+    }
+    else {
+        errormsg.textContent = 'Please provide a valid email address';
+        errormsg.style.color = 'red';
     }
 
-    else{
-        msgText.innerHTML = 'An email has been sent to you, thanks for submiting';
-        msgText.style.color = 'green';
-        emailInput.classList.remove('redBox');
-    }
 
-})
+});
+
+//Submit button checks for entred format if valid or invalid thne proceeds
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const isValid = email.value === 0 || emailRegex.test(email.value);
+
+    if (!isValid) {
+        errormsg.textContent = 'Please provide a valid email address';
+        
+
+    }
+    else {
+        errormsg.textContent = 'An email has been sent to you, thanks for submitting';
+        errormsg.style.color = 'green';
+        
+    }
+});
+
 
