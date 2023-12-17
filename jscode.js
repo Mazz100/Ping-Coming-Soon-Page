cbc630312a95883701abade47a01d4ab167acfba
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2023-12-17 19:33:07 +0200

    Feedback Report Update
    - Now the validation only checks when submitted and the error message disappear when user enters correct format.
    
    - Improved mobile view and responsivness.

--- jscode.js
+++ jscode.js
@@ -7,27 +7,16 @@ const textmsg = errormsg.nextElementSibling;
 
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-window.addEventListener('load', () => {
-
-    //Testing the inputfield if its empty | if not empty check for correct email formatting
-    const isValid = email.value === 0 || emailRegex.test(email.value);
-    //Condition context: if isValid true then valid else invalid
-    email.className = isValid ? "valid" : "invalid";
-
-});
-
 //Check for user input if not empty or has required regex then remove errormsg
 email.addEventListener('input', () => {
     const isValid = email.value.length === 0 || emailRegex.test(email.value);
     if (isValid) {
-        email.className = "valid";
         errormsg.textContent = '';
         inputBorder.style.borderColor = ' hsl(223, 100%, 88%)';
     }
     else {
-        inputBorder.style.borderColor = ' hsl(354, 100%, 66%)';
-        errormsg.textContent = 'Please provide a valid email address';
-        textmsg.textContent = '';
+        //Checks and returns if input is valid after submitting form
+       return isValid;
     }
 
 
